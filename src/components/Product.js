5f124e04cec3ac07b6a99b97e2919144c70a859e
Hakan <hakan9719@gmail.com>
2021-12-15 13:18:23 +0100

    slice products

--- src/components/Product.js
+++ src/components/Product.js
@@ -18,8 +18,8 @@ export default function Product({
   const [hasPrime, setHasPrime] = useState(Math.random() < 0.5);
 
   return (
-    <div>
-      <p>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="asolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
       <Image
         src={image}
         height={200}
@@ -27,16 +27,25 @@ export default function Product({
         objectFit="contain"
         alt={title}
       />
-      <h4>{title}</h4>
+      <h4 className="my-3">{title}</h4>
       <div className="flex">
         {Array(rating)
           .fill()
           .map((_, i) => (
-            <StarIcon key={i} className="h-5" />
+            <StarIcon key={i} className="h-5 text-yellow-500" />
           ))}
       </div>
-      <p>{description}</p>
-      <div></div>
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+      <div className="mb-5">
+        <p>{price}€</p>
+      </div>
+      {hasPrime && (
+        <div className="flex items-center space-x-2 -mt-5 ">
+          <img className="w-12" src={"https://links.papareact.com/fdw"} alt="Prime" />
+          <p className="text-sm text-gray-500">Free Next Day Delivery</p>
+        </div>
+      )}
+      <button className="mt-auto button">Add to Basket</button>
     </div>
   );
 }
